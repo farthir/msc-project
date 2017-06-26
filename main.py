7362ff9ee512dc425234d2e55a2be66d9caa157b
Rob Farthing <farthir@outlook.com>
2017-06-26 18:12:37 +0100

    finish backdrop loop and start testing with xor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # driver module for programme
 import mlp_functions
 import config
-from pprint import pprint
+import math
+import matplotlib.pyplot as plt
 
 # read in patterns
 patterns = mlp_functions.read_patterns('data/xor.csv')
@@ -22,22 +23,6 @@ elif (config.params['validating'] and
 elif not config.params['validating']:
     training_patterns = patterns
 
-# separate input vector from teaching/output vector
-# this entire section can probably be removed
-training_x = []
-training_t = []
-for p in training_patterns:
-    training_x.append(p[:config.params['input_dimensions']])
-    training_t.append(p[config.params['input_dimensions']:])
-
-if config.params['validating']:
-    # separate input vector from teaching/output vector
-    validation_x = []
-    validation_t = []
-    for p in validation_patterns:
-        validation_x.append(p[:config.params['input_dimensions']])
-        validation_t.append(p[config.params['input_dimensions']:])
-
 # network initialisation
 # should probably create a neuron class to do this properly as it's a mess
 # set number of neurons in each layer
@@ -52,22 +37,23 @@ if config.params['neuron_layers'] > 1:
 # this may not always be the case but is set here
 neurons_l.append(config.params['output_dimensions'])
 
-# get weights
+# initialise weights
 weights_l_i_j = mlp_functions.initialise_weights(neurons_l)
 
 # backpropagation loop
 epoch = 0
-
-if config.params['validating']:
-    error_validation_prev = 1000.0
-
+training_errors = []
 repeat = True
+if config.params['validating']:
+    validation_error_best = 1000.0
+    validation_errors = []
+
 while (repeat):
-    error = 0.0
+    training_error = 0.0
+
     for p in training_patterns:
         # load pattern
         input_pattern = p[:config.params['input_dimensions']]
-        outputs_l_j = []
         # set bias 'output'
         outputs_l_j = mlp_functions.initialise_bias(
                         config.params['neuron_layers'])
@@ -78,13 +64,77 @@ while (repeat):
         outputs_l_j = mlp_functions.forward_pass(neurons_l, weights_l_i_j,
                                                  outputs_l_j)
 
-    if (error < config.params['target_training_error'] or
+        # update training_error
+        output_pattern = p[config.params['input_dimensions']:]
+        teacher_i = []
+        # account for i = 0
+        teacher_i.append(None)
+        teacher_i.extend(output_pattern)
+        training_error = mlp_functions.update_ms_error(
+                            neurons_l, training_error, teacher_i, outputs_l_j)
+
+        # calculate errors
+        errors_l_i = mlp_functions.calculate_errors(neurons_l, weights_l_i_j,
+                                                    teacher_i, outputs_l_j)
+
+        # update weights
+        weights_l_i_j = mlp_functions.update_weights(neurons_l, weights_l_i_j,
+                                                     errors_l_i, outputs_l_j)
+
+    # normalise mean squared training error into [0,1] and convert to rms
+    training_error = math.sqrt(training_error / (neurons_l[-1] *
+                                                 len(training_patterns)))
+
+    # write out epoch training_error
+    training_errors.append(training_error)
+
+    if config.params['validating']:
+        validation_error = 0.0
+
+        for p in validation_patterns:
+            # load pattern
+            input_pattern = p[:config.params['input_dimensions']]
+            # set bias 'output'
+            outputs_l_j = mlp_functions.initialise_bias(
+                            config.params['neuron_layers'])
+            # add input pattern to 'output' of layer 0 (i.e. set the input to p
+            outputs_l_j[0].extend(input_pattern)
+
+            # forward pass
+            outputs_l_j = mlp_functions.forward_pass(neurons_l, weights_l_i_j,
+                                                     outputs_l_j)
+
+            # update validation error
+            output_pattern = p[config.params['input_dimensions']:]
+            teacher_i = []
+            # account for i = 0
+            teacher_i.append(None)
+            teacher_i.extend(output_pattern)
+            validation_error = mlp_functions.update_ms_error(
+                        neurons_l, validation_error, teacher_i, outputs_l_j)
+
+        # normalise mean squared validation error into [0,1] and convert to rms
+        validation_error = math.sqrt(validation_error / (neurons_l[-1] * (
+                                                    len(validation_patterns))))
+
+        # make sure validation error is dropping
+        if (validation_error < validation_error_best):
+            validation_error_best = validation_error
+            # mlp_functions.copy_weights_to_best_on_val()
+
+        validation_errors.append(validation_error)
+
+    # temp
+    print('te: ', training_error)
+    # print('ve: ', validation_error)
+
+    epoch += 1
+    if (training_error < config.params['target_training_error'] or
             epoch == config.params['max_epochs']):
         repeat = False
 
 # temp
-pprint(config.params)
-print(neurons_l)
-print(weights_l_i_j)
-print('after forward pass')
-print(outputs_l_j)
+plt.plot(training_errors, 'bs')
+plt.xlabel('Epoch')
+plt.ylabel('Error')
+# plt.show()
--- mlp_functions.py
+++ mlp_functions.py
@@ -65,6 +65,7 @@ def initialise_weights(neurons_l):
 
 
 def forward_pass(neurons_l, weights_l_i_j, outputs_l_j):
+    L = len(neurons_l) - 1
     for l, neuron_l in enumerate(neurons_l):
         # skip first layer as it's already populated and has no neurons
         if (l != 0):
@@ -78,13 +79,13 @@ def forward_pass(neurons_l, weights_l_i_j, outputs_l_j):
                     activation += (weights_l_i_j[l][i][j] *
                                    outputs_l_j[l-1][j])
                 # set outputs in hidden layers
-                if (l < (len(neurons_l) - 1)):
+                if (l < L):
                     # apply squashing to hidden layers
                     firing_function = get_firing_function(
                         config.params['hidden_layers_function'])
                     outputs_l_j[l].append(firing_function(activation))
                 # set outputs in output layer
-                elif (l == (len(neurons_l) - 1)):
+                elif (l == L):
                     # check output layer function
                     if (config.params['output_function'] == 'linear'):
                         # linear output
@@ -116,3 +117,84 @@ def get_firing_function(function_name):
                     (math.exp(activation) + math.exp(-activation))), 2))
 
     return firing_function
+
+
+def update_ms_error(neurons_l, error, teacher_i, outputs_l_j):
+    # get number of output layer neurons
+    L = len(neurons_l) - 1
+    output_neurons = neurons_l[L]
+    for i in range(output_neurons):
+        # neuron count starts at 1
+        i += 1
+        error += math.pow(teacher_i[i] - outputs_l_j[L][i], 2)
+    return error
+
+
+def calculate_errors(neurons_l, weights_l_i_j, teacher_i, outputs_l_j):
+    L = len(neurons_l) - 1
+    reversed_errors_l_i = []
+    for l, neuron_l in reversed(list(enumerate(neurons_l))):
+        # skip first layer as it's the input layer
+        reversed_errors_i = []
+        # indexing starts at 1 for neurons
+        reversed_errors_i.append(None)
+
+        if (l != 0):
+            for i in range(neuron_l):
+                # neuron count starts at 1
+                i += 1
+                # calculate activation
+                activation = 0.0
+                # plus one for bias
+                for j in range(neurons_l[l - 1] + 1):
+                    activation += (weights_l_i_j[l][i][j] *
+                                   outputs_l_j[l-1][j])
+                # output layer with known targets
+                if (l == L):
+                    # check output layer function
+                    difference = 0.0
+                    difference = teacher_i[i] - outputs_l_j[l][i]
+                    if (config.params['output_function'] == 'linear'):
+                        # linear output
+                        reversed_errors_i.append(difference)
+                    else:
+                        # squashed output
+                        firing_function = get_firing_function(
+                            (config.params['output_function'] + '_derivative'))
+                        reversed_errors_i.append(difference *
+                                                 firing_function(activation))
+                # hidden layers
+                else:
+                    # calculate sum
+                    error_sum = 0.0
+                    # start at 1
+                    for k in range(neurons_l[l + 1]):
+                        # neuron count starts at 1
+                        k += 1
+                        # reversed_errors_i is backward
+                        error_sum += (reversed_errors_l_i[L - (l + 1)][k] *
+                                      weights_l_i_j[l + 1][k][i])
+                    # hidden layers squashing
+                    firing_function = get_firing_function(
+                       config.params['hidden_layers_function'] + '_derivative')
+                    reversed_errors_i.append(error_sum *
+                                             firing_function(activation))
+        reversed_errors_l_i.append(reversed_errors_i)
+
+    return reversed_errors_l_i[::-1]
+
+
+def update_weights(neurons_l, weights_l_i_j, errors_l_i, outputs_l_j):
+        for l, neuron_l in enumerate(neurons_l):
+            # skip first layer as it has no neurons
+            if (l != 0):
+                for i in range(neuron_l):
+                    # neuron count starts at 1
+                    i += 1
+                    # plus one for bias
+                    for j in range(neurons_l[l - 1] + 1):
+                        weights_l_i_j[l][i][j] += weights_l_i_j[l][i][j] + (
+                            config.params['training_rate'] *
+                            errors_l_i[l][i] *
+                            outputs_l_j[l - 1][j])
+        return weights_l_i_j
